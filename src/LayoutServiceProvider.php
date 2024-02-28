0681887481501177cdc15a7380d605adfbe29959
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-28 23:25:55 +0330

    add layout service provider

---
+++ src/LayoutServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JobMetric\Layout;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class LayoutServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-layout')
+            ->hasConfig()
+            ->hasMigration()
+            ->registerClass('Layout', Layout::class);
+    }
+}
