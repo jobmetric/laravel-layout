bbea4eacaf932839694ef30b321272485fee0326
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-28 23:26:41 +0330

    add layout service

---
+++ src/Layout.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace JobMetric\Layout;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class Layout
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * Create a new Setting instance.
+     *
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+}
