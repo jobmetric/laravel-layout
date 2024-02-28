913b6b042b1dffa6d9150604a22f6760a012d38f
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-28 23:22:54 +0330

    add config

---
+++ config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data layout
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("LAYOUT_CACHE_TIME", 0),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'layout' => 'layouts',
+        'layout_extension' => 'layout_extensions',
+        'layout_page' => 'layout_pages'
+    ],
+
+];
