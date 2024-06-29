c201c935ddcc53d3cdda859b2f2c06542231b6b3
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-29 19:51:35 +0330

    add base language file

---
+++ lang/en/base.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Layout Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Layout for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+    ],
+
+    'messages' => [
+        'created' => 'The layout was created successfully.',
+        'updated' => 'The layout was updated successfully.',
+        'deleted' => 'The layout was deleted successfully.',
+    ],
+
+];
