21396b2cb0d86444a9ba01f6f173edef93ed7c0b
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-30 01:50:43 +0330

    add all crud method in layout service and facade

--- src/Facades/Layout.php
+++ src/Facades/Layout.php
@@ -7,9 +7,15 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \JobMetric\Layout\Layout
  *
- * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [], array $with = [], string $mode = 'withTrashed')
- * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = 'withTrashed')
- * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(array $filter = [], array $with = [], string $mode = 'withTrashed')
+ * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [], array $with = [], string $mode = null)
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = null)
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(array $filter = [], array $with = [], string $mode = null)
+ * @method static array get(int $layout_id, array $with = [], string $mode = null)
+ * @method static array store(array $data)
+ * @method static array update(int $layout_id, array $data = [])
+ * @method static array delete(int $layout_id)
+ * @method static array restore(int $layout_id)
+ * @method static array forceDelete(int $layout_id)
  */
 class Layout extends Facade
 {
--- src/Layout.php
+++ src/Layout.php
@@ -4,11 +4,19 @@ namespace JobMetric\Layout;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Pagination\LengthAwarePaginator;
-use JobMetric\Extension\Http\Resources\ExtensionResource;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use JobMetric\Layout\Events\LayoutDeleteEvent;
+use JobMetric\Layout\Events\LayoutForceDeleteEvent;
+use JobMetric\Layout\Events\LayoutRestoreEvent;
+use JobMetric\Layout\Events\LayoutStoreEvent;
+use JobMetric\Layout\Events\LayoutUpdateEvent;
+use JobMetric\Layout\Http\Requests\StoreLayoutRequest;
+use JobMetric\Layout\Http\Requests\UpdateLayoutRequest;
 use JobMetric\Layout\Http\Resources\LayoutResource;
 use JobMetric\Layout\Models\Layout as LayoutModel;
 use Spatie\QueryBuilder\QueryBuilder;
+use Throwable;
 
 class Layout
 {
@@ -34,11 +42,11 @@ class Layout
      *
      * @param array $filter
      * @param array $with
-     * @param string $mode
+     * @param string|null $mode
      *
      * @return QueryBuilder
      */
-    private function query(array $filter = [], array $with = [], string $mode = 'withTrashed'): QueryBuilder
+    private function query(array $filter = [], array $with = [], string $mode = null): QueryBuilder
     {
         $fields = ['id', 'name', 'status', 'created_at', 'updated_at'];
 
@@ -48,7 +56,7 @@ class Layout
             $query->withTrashed();
         }
 
-        if($mode === 'onlyTrashed') {
+        if ($mode === 'onlyTrashed') {
             $query->onlyTrashed();
         }
 
@@ -73,11 +81,11 @@ class Layout
      * @param array $filter
      * @param int $page_limit
      * @param array $with
-     * @param string $mode
+     * @param string|null $mode
      *
      * @return AnonymousResourceCollection
      */
-    public function paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = 'withTrashed'): AnonymousResourceCollection
+    public function paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = null): AnonymousResourceCollection
     {
         return LayoutResource::collection(
             $this->query($filter, $with, $mode)->paginate($page_limit)
@@ -89,14 +97,300 @@ class Layout
      *
      * @param array $filter
      * @param array $with
-     * @param string $mode
+     * @param string|null $mode
      *
      * @return AnonymousResourceCollection
      */
-    public function all(array $filter = [], array $with = [], string $mode = 'withTrashed'): AnonymousResourceCollection
+    public function all(array $filter = [], array $with = [], string $mode = null): AnonymousResourceCollection
     {
         return LayoutResource::collection(
             $this->query($filter, $with, $mode)->get()
         );
+    }
+
+    /**
+     * Get the specified layout.
+     *
+     * @param int $layout_id
+     * @param array $with
+     * @param string|null $mode
+     *
+     * @return array
+     */
+    public function get(int $layout_id, array $with = [], string $mode = null): array
+    {
+        if ($mode === 'withTrashed') {
+            $query = LayoutModel::withTrashed();
+        } else if ($mode === 'onlyTrashed') {
+            $query = LayoutModel::onlyTrashed();
+        } else {
+            $query = LayoutModel::query();
+        }
+
+        $query->where('id', $layout_id);
+
+        if (!empty($with)) {
+            $query->with($with);
+        }
+
+        $layout = $query->first();
+
+        if (!$layout) {
+            return [
+                'ok' => false,
+                'message' => trans('layout::base.validation.errors'),
+                'errors' => [
+                    trans('layout::base.validation.object_not_found')
+                ],
+                'status' => 404
+            ];
+        }
+
+        return [
+            'ok' => true,
+            'message' => trans('layout::base.messages.found'),
+            'data' => LayoutResource::make($layout),
+            'status' => 200
+        ];
+    }
+
+    /**
+     * Store the specified layout.
+     *
+     * @param array $data
+     *
+     * @return array
+     * @throws Throwable
+     */
+    public function store(array $data): array
+    {
+        $validator = Validator::make($data, (new StoreLayoutRequest)->rules());
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+
+            return [
+                'ok' => false,
+                'message' => trans('layout::base.validation.errors'),
+                'errors' => $errors,
+                'status' => 422
+            ];
+        } else {
+            $data = $validator->validated();
+
+            return DB::transaction(function () use ($data) {
+                $layout = new LayoutModel;
+                $layout->name = $data['name'];
+                $layout->status = $data['status'];
+
+                $layout->save();
+
+                $layout->layoutPages()->createMany($data['pages']);
+                $layout->layoutPlugins()->createMany($data['plugins']);
+
+                event(new LayoutStoreEvent($layout, $data));
+
+                return [
+                    'ok' => true,
+                    'message' => trans('layout::base.messages.created'),
+                    'data' => LayoutResource::make($layout),
+                    'status' => 201
+                ];
+            });
+        }
+    }
+
+    /**
+     * Update the specified layout.
+     *
+     * @param int $layout_id
+     * @param array $data
+     *
+     * @return array
+     */
+    public function update(int $layout_id, array $data = []): array
+    {
+        $validator = Validator::make($data, (new UpdateLayoutRequest)->setLayoutId($layout_id)->rules());
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+
+            return [
+                'ok' => false,
+                'message' => trans('layout::base.validation.errors'),
+                'errors' => $errors,
+                'status' => 422
+            ];
+        } else {
+            $data = $validator->validated();
+
+            return DB::transaction(function () use ($layout_id, $data) {
+                /**
+                 * @var LayoutModel $layout
+                 */
+                $layout = LayoutModel::query()->where('id', $layout_id)->first();
+
+                if (!$layout) {
+                    return [
+                        'ok' => false,
+                        'message' => trans('layout::base.validation.errors'),
+                        'errors' => [
+                            trans('layout::base.validation.object_not_found')
+                        ],
+                        'status' => 404
+                    ];
+                }
+
+                if (array_key_exists('name', $data)) {
+                    $layout->name = $data['name'];
+                }
+
+                if (array_key_exists('status', $data)) {
+                    $layout->status = $data['status'];
+                }
+
+                $layout->save();
+
+                if (array_key_exists('pages', $data)) {
+                    $layout->layoutPages()->delete();
+                    $layout->layoutPages()->createMany($data['pages']);
+                }
+
+                if (array_key_exists('plugins', $data)) {
+                    $layout->layoutPlugins()->delete();
+                    $layout->layoutPlugins()->createMany($data['plugins']);
+                }
+
+                event(new LayoutUpdateEvent($layout, $data));
+
+                return [
+                    'ok' => true,
+                    'message' => trans('layout::base.messages.updated'),
+                    'data' => LayoutResource::make($layout),
+                    'status' => 200
+                ];
+            });
+        }
+    }
+
+    /**
+     * Delete the specified layout and send to trash.
+     *
+     * @param int $layout_id
+     *
+     * @return array
+     */
+    public function delete(int $layout_id): array
+    {
+        return DB::transaction(function () use ($layout_id) {
+            /**
+             * @var LayoutModel $layout
+             */
+            $layout = LayoutModel::query()->where('id', $layout_id)->first();
+
+            if (!$layout) {
+                return [
+                    'ok' => false,
+                    'message' => trans('layout::base.validation.errors'),
+                    'errors' => [
+                        trans('layout::base.validation.object_not_found')
+                    ],
+                    'status' => 404
+                ];
+            }
+
+            event(new LayoutDeleteEvent($layout));
+
+            $data = LayoutResource::make($layout);
+
+            $layout->delete();
+
+            return [
+                'ok' => true,
+                'data' => $data,
+                'message' => trans('layout::base.messages.deleted'),
+                'status' => 200
+            ];
+        });
+    }
+
+    /**
+     * Restore the specified layout.
+     *
+     * @param int $layout_id
+     *
+     * @return array
+     */
+    public function restore(int $layout_id): array
+    {
+        return DB::transaction(function () use ($layout_id) {
+            /**
+             * @var LayoutModel $layout
+             */
+            $layout = LayoutModel::onlyTrashed()->where('id', $layout_id)->first();
+
+            if (!$layout) {
+                return [
+                    'ok' => false,
+                    'message' => trans('layout::base.validation.errors'),
+                    'errors' => [
+                        trans('layout::base.validation.object_not_found')
+                    ],
+                    'status' => 404
+                ];
+            }
+
+            event(new LayoutRestoreEvent($layout));
+
+            $data = LayoutResource::make($layout);
+
+            $layout->restore();
+
+            return [
+                'ok' => true,
+                'data' => $data,
+                'message' => trans('layout::base.messages.restored'),
+                'status' => 200
+            ];
+        });
+    }
+
+    /**
+     * Force delete the specified layout.
+     *
+     * @param int $layout_id
+     *
+     * @return array
+     */
+    public function forceDelete(int $layout_id): array
+    {
+        return DB::transaction(function () use ($layout_id) {
+            /**
+             * @var LayoutModel $layout
+             */
+            $layout = LayoutModel::onlyTrashed()->where('id', $layout_id)->first();
+
+            if (!$layout) {
+                return [
+                    'ok' => false,
+                    'message' => trans('layout::base.validation.errors'),
+                    'errors' => [
+                        trans('layout::base.validation.object_not_found')
+                    ],
+                    'status' => 404
+                ];
+            }
+
+            event(new LayoutForceDeleteEvent($layout));
+
+            $data = LayoutResource::make($layout);
+
+            $layout->forceDelete();
+
+            return [
+                'ok' => true,
+                'data' => $data,
+                'message' => trans('layout::base.messages.permanently_deleted'),
+                'status' => 200
+            ];
+        });
     }
 }
