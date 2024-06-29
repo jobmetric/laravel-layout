a6071be920b96613aa8fa6bd8af1a75e88fa1ec1
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-29 20:59:04 +0330

    add method query and paginate and all in layout service

--- src/Facades/Layout.php
+++ src/Facades/Layout.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \JobMetric\Layout\Layout
  *
- * @method static array store(array $data)
- * @method static array update(int $layout_id, array $data)
- * @method static array delete(int $layout_id)
+ * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [], array $with = [], string $mode = 'withTrashed')
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = 'withTrashed')
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(array $filter = [], array $with = [], string $mode = 'withTrashed')
  */
 class Layout extends Facade
 {
@@ -20,6 +20,6 @@ class Layout extends Facade
      */
     protected static function getFacadeAccessor(): string
     {
-        return 'Layout';
+        return \JobMetric\Layout\Layout::class;
     }
 }
--- src/Layout.php
+++ src/Layout.php
@@ -3,6 +3,12 @@
 namespace JobMetric\Layout;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use JobMetric\Extension\Http\Resources\ExtensionResource;
+use JobMetric\Layout\Http\Resources\LayoutResource;
+use JobMetric\Layout\Models\Layout as LayoutModel;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class Layout
 {
@@ -21,5 +27,76 @@ class Layout
     public function __construct(Application $app)
     {
         $this->app = $app;
+    }
+
+    /**
+     * Get the specified layout.
+     *
+     * @param array $filter
+     * @param array $with
+     * @param string $mode
+     *
+     * @return QueryBuilder
+     */
+    private function query(array $filter = [], array $with = [], string $mode = 'withTrashed'): QueryBuilder
+    {
+        $fields = ['id', 'name', 'status', 'created_at', 'updated_at'];
+
+        $query = QueryBuilder::for(LayoutModel::class);
+
+        if ($mode === 'withTrashed') {
+            $query->withTrashed();
+        }
+
+        if($mode === 'onlyTrashed') {
+            $query->onlyTrashed();
+        }
+
+        $query->allowedFields($fields)
+            ->allowedSorts($fields)
+            ->allowedFilters($fields)
+            ->defaultSort([
+                '-id'
+            ])
+            ->where($filter);
+
+        if (!empty($with)) {
+            $query->with($with);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Paginate the specified layout.
+     *
+     * @param array $filter
+     * @param int $page_limit
+     * @param array $with
+     * @param string $mode
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = 'withTrashed'): AnonymousResourceCollection
+    {
+        return LayoutResource::collection(
+            $this->query($filter, $with, $mode)->paginate($page_limit)
+        );
+    }
+
+    /**
+     * Get all layouts.
+     *
+     * @param array $filter
+     * @param array $with
+     * @param string $mode
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function all(array $filter = [], array $with = [], string $mode = 'withTrashed'): AnonymousResourceCollection
+    {
+        return LayoutResource::collection(
+            $this->query($filter, $with, $mode)->get()
+        );
     }
 }
