aceaf664b027c96bafba57e73130391d09bbb189
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-29 19:53:37 +0330

    change event files

--- src/Events/LayoutDeleteEvent.php
+++ src/Events/LayoutDeleteEvent.php
@@ -2,15 +2,10 @@
 
 namespace JobMetric\Layout\Events\Layout;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 use JobMetric\Layout\Models\Layout;
 
 class LayoutDeleteEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
     /**
      * Create a new event instance.
      */
